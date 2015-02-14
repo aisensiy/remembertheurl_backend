18684e90073982de223887197da7819e4eab4f5b
aisensiy <aisensiy@gmail.com>
2015-02-14 19:53:06 +0800

    create batch create view api

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -4,7 +4,24 @@ RSpec.describe ApiController, type: :controller do
 
   describe "POST #create" do
     it "returns http success" do
-      post :create
+      data = [
+        {
+          title: 'example',
+          url: 'http://example.org',
+          start_ts: 1423889149724,
+          duration: 465147
+        },
+        {
+          title: 'example2',
+          url: 'http://example.com',
+          start_ts: 1423911502321,
+          duration: 4647
+        }
+      ]
+      count_before = View.count
+      post :create, data: data.to_json
+      count_after = View.count
+      expect(count_after).to eq count_before + data.size
       expect(response).to have_http_status(:created)
     end
   end
