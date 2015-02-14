dfaf73ff15bd321766e1712493bbf30b0d0a60fb
aisensiy <aisensiy@gmail.com>
2015-02-14 19:52:41 +0800

    create view model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150214112140) do
+
+  create_table "views", force: true do |t|
+    t.text     "url"
+    t.text     "title"
+    t.datetime "start_at"
+    t.integer  "duration"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
